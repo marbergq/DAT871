3cda097ba8feedf09bbd8f6c275d16642d4ae05d
Martin Bergqvist <martin@ourstudio.se>
2018-04-12 14:56:45 +0200

    Lab1.1v2

--- lab1/mp-pi-montecarlo-pool.py
+++ lab1/mp-pi-montecarlo-pool.py
@@ -3,34 +3,41 @@ import argparse # See https://docs.python.org/2/howto/argparse.html for a tutori
 import random
 from math import pi
 
-def sample_pi(id, queue):
+def sample_pi(n):
     """ Perform n steps of Monte Carlo simulation for estimating Pi/4.
         Returns the number of sucesses."""
     random.seed()
-    while True:
+    s = 0
+    for _ in range(n):
         x = random.random()
         y = random.random()
         if x**2 + y**2 <= 1.0:
-            queue.put(1)
-        else:
-            queue.put(0)
+            s += 1
+    return s
+
+def worker(batch_size, queue):
+    while True:
+        s = sample_pi(batch_size)
+        queue.put(s)
 
 def compute_pi(args):
+    batch_size = 1000
     results = multiprocessing.Queue()
     workers = []
 
-    for id in range(args.workers):
-        workers.append(multiprocessing.Process(target=sample_pi, args=(id,results)))
+    for _ in range(args.workers):
+        workers.append(multiprocessing.Process(target=worker, args=(batch_size, results)))
     for w in workers:
         w.start()
 
     n_total = 0
     s_total = 0
     pi_est = 0
-
-    while abs(pi-pi_est) > 1 / float(args.accuracy):
+    accuracy = 1 / float(args.accuracy)
+    
+    while abs(pi-pi_est) > accuracy:
         s_total += results.get()
-        n_total += 1
+        n_total += batch_size
         pi_est = (4.0*s_total)/n_total
     
     for w in workers:
@@ -38,7 +45,7 @@ def compute_pi(args):
         w.join()
     
     print " Steps\tSuccess\tPi est.\tError"
-    print "%6d\t%7d\t%1.5f\t%1.5f" % (n_total, s_total, pi_est, pi-pi_est)
+    print "%6d\t%7d\t%1.10f\t%1.10f" % (n_total, s_total, pi_est, pi-pi_est)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Compute Pi using Monte Carlo simulation.')
